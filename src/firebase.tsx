3393379635702a42363b169f8aa5aed396ffcf6c
Alden Parker <ajparker1401@gmail.com>
2020-05-03 12:34:29 -0700

    some basic work

---
+++ src/firebase.tsx
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB39x2WEDnWHxUb_TXaPoDh72dnUFWTD2Y",
+    authDomain: "covid-digest.firebaseapp.com",
+    databaseURL: "https://covid-digest.firebaseio.com",
+    projectId: "covid-digest",
+    storageBucket: "covid-digest.appspot.com",
+    messagingSenderId: "740953347752",
+    appId: "1:740953347752:web:f3455676b98f87de17949d",
+    measurementId: "G-MHVT56HLB8"
+};
+
+firebase.initializeApp(firebaseConfig);
+export default firebase;
---
+++ src/home/home.tsx
@@ -0,0 +1,91 @@
+import React, {Component} from 'react';
+import './home.scss';
+// import SignUpCard from "../components/signupcard/signupcard"
+
+const features = [
+  {
+    title: <>No More Clickbait Headlines</>,
+    imageUrl: '../../public/img/guy_on_mound.svg',
+    description: (
+        <>
+          You only get the information that matters in this time of crisis, hard news about COVID, without the clickbait or politics. No more scrolling through news apps looking for information, it gets delivered to your inbox three times a week.
+        </>
+    ),
+  },
+  {
+    title: <>Bite Size Segments</>,
+    imageUrl: '../../public/img/girl_with_notifications.svg',
+    description: (
+        <>
+          We give you bite size pieces of news to prevent being mentally overwhelemed by the flow of information in todays news cycles. Our algorithm automatically summarizes the article’s key information, to make it easier for your eyes and your mind.
+        </>
+    ),
+  },
+  {
+    title: <>Transparent Sources</>,
+    imageUrl: '../../public/img/computer.svg',
+    description: (
+        <>
+          This whole project is open sourced, so you can see where the information is coming from and how it’s being sumarized.
+        </>
+    ),
+  },
+];
+
+class Feature extends Component<{ imageUrl: any, title: any, description: any }> {
+    render() {
+        let {imageUrl, title, description} = this.props;
+        return (
+            <div className="col col--4 feature">
+                {imageUrl && (
+                    <div className="text--center">
+                        <img className="featureImage" src={imageUrl} alt={title}/>
+                    </div>
+                )}
+                <h2 className="text--center featureTitle">{title}</h2>
+                <p className="text--center featureText">{description}</p>
+            </div>
+        );
+    }
+}
+
+function Home() {
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col col--6">
+            <h1 className="headerText">
+              COVID19 News Updates. <br/>
+              Delivered via SMS.
+            </h1>
+            <button className = "signUpButton">GET YOUR UPDATES</button>
+          </div>
+          <div className="col col--6">
+            <img className="iphone" src="../../public/img/iphone.svg" alt="iPhone" />
+          </div>
+        </div>
+      </div>
+      <div>
+        {features && features.length && (
+            <section className="features">
+              <div className="container">
+                <div className="row flex justify-center -align-center">
+                  {features.map((props, idx) => (
+                      <Feature key={idx} {...props} />
+                  ))}
+                </div>
+              </div>
+            </section>
+        )}
+        <section className="signUpForm">
+          <div className="container">
+            {/*<SignUpCard/>*/}
+          </div>
+        </section>
+      </div>
+    </>
+  );
+}
+
+export default Home;
